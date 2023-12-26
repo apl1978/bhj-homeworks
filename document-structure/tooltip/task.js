bcb07561d75769b35bc79c66dfa34c98b0198324
Alexey Pirozhkov <apl_1978@yahoo.com>
2023-12-26 10:13:05 +0300

    Update task.js
    Rework

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,24 +1,24 @@
 const tooltips = Array.from(document.querySelectorAll(".has-tooltip"));
+
+let tip = document.createElement("div");
+tip.className = "tooltip";
+tip.textContent = "";
+tip.style.top = "0px";
+tip.style.left = "0px";
+
+document.body.append(tip);
+
 tooltips.forEach((tooltip) => {
   tooltip.addEventListener("click", function (event) {
     event.preventDefault();
-    let tip = document.createElement("div");
-    tip.className = "tooltip";
-    tip.textContent = this.title;
-    tip.classList.add("tooltip_active");
+    tip.classList.remove("tooltip_active");
 
-    coord = this.getBoundingClientRect();
-
-    tip.style.top = coord.top + coord.height + "px";
-    tip.style.left = coord.left + "px";
-
-    document.body.append(tip);
-  });
-
-  tooltip.addEventListener("mouseout", function (event) {
-    const tips = Array.from(document.querySelectorAll(".tooltip"));
-    tips.forEach((tip) => {
-      tip.classList.remove("tooltip_active");
-    });
+    if (tip.textContent != this.title) {
+      tip.textContent = this.title;
+      coord = this.getBoundingClientRect();
+      tip.style.top = coord.top + coord.height + "px";
+      tip.style.left = coord.left + "px";
+      tip.classList.add("tooltip_active");
+    }
   });
 });
