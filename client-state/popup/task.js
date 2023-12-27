2af5566f08a3f28cd6d8da39a0045837c9a1ecdb
Alexey Pirozhkov <apl_1978@yahoo.com>
2023-12-27 09:45:01 +0300

    Update task.js

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,6 +1,5 @@
 const modalWindow = document.getElementById("subscribe-modal");
 const closeBtn = document.querySelector(".modal__close");
-modalWindow.classList.add("modal_active");
 
 closeBtn.onclick = function () {
   document.cookie = "modal__close=yes";
@@ -10,9 +9,12 @@ closeBtn.onclick = function () {
 function getCookie(name) {
   const pairs = document.cookie.split("; ");
   cookie = pairs.find((p) => p.startsWith(name + "="));
-  return cookie.substr(name.length + 1);
+  if (cookie) {
+    return cookie.substr(name.length + 1);
+  }
+  return undefined;
 }
 
-if (getCookie("modal__close") == "yes") {
-  modalWindow.classList.remove("modal_active");
+if (getCookie("modal__close") != "yes") {
+  modalWindow.classList.add("modal_active");
 }
